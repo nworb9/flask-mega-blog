14a26282925e1ead35eb0ab645d342afb7900371
Emma Brown <nworb95@gmail.com>
2020-09-13 17:16:50 -0400

    Added flask db and migrations repo

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -24,5 +25,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
